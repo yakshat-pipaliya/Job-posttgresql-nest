00db70798c9c413645be3101dec93220000863ce
yakshat <pipaliyayakshat@gmail.com>
2025-05-22 18:10:57 +0530

    nine commit

--- src/common/login.interceptor.ts
+++ src/common/login.interceptor.ts
@@ -1,18 +1,6 @@
 import { Injectable, NestInterceptor, ExecutionContext, CallHandler, } from '@nestjs/common';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
-
-// @Injectable()
-// export class LoginInterceptor implements NestInterceptor {
-//   intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
-//     const now = Date.now();
-//     return next
-//       .handle()
-//       .pipe(
-//         tap(() => console.log(`Request took ${Date.now() - now}ms`))
-//       );
-//   }
-// }
 @Injectable()
 export class LoginInterceptor implements NestInterceptor {
     intercept(context: ExecutionContext, next: CallHandler): Observable<any> {
